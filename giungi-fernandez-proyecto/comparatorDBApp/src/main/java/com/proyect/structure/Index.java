68007079c48f1b1513d21e11f1f0f91f2a8b96ca
tomas98Fer <tomas016fernandez@gmail.com>
2024-10-09 16:31:41 -0300

    Add Implementation equals method in Index Class.

--- giungi-fernandez-proyecto/comparatorDBApp/src/main/java/com/proyect/structure/Index.java
+++ giungi-fernandez-proyecto/comparatorDBApp/src/main/java/com/proyect/structure/Index.java
@@ -31,10 +31,29 @@ public class Index extends  MetaObjectDB {
 	 * 
 	 * @return columns of index;
 	 */
-	public ArrayList<Column> getColumnsKey(){
+	public ArrayList<Column> getColumns(){
 		
 		return this.columns;
 	}
 	
+	
+	@Override
+	public boolean equals(Object o ) {
+		if(o instanceof Index  ) {
+			Index idx = (Index) o;
+			if(idx.getColumns().size() != this.columns.size()) 
+				return false;
+			for(int i = 0; i < this.columns.size();i++) {
+				String type_idx = idx.getColumns().get(i).getType();
+				String type_this = this.columns.get(i).getType();
+				if(!type_idx.equals(type_this)) {
+					return false;
+				}
+				
+			}
+			return true;
+		}
+		return false;
+	}
 	
 }
