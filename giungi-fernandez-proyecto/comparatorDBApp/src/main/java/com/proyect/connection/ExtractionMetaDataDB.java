d0a5c88b68acdf92d5d25f4d238a90ee603e3c48
tomas98Fer <tomas016fernandez@gmail.com>
2024-10-18 13:39:58 -0300

    refactor and implement methods to obtain primary and foreign keys metadata.

--- giungi-fernandez-proyecto/comparatorDBApp/src/main/java/com/proyect/connection/ExtractionMetaDataDB.java
+++ giungi-fernandez-proyecto/comparatorDBApp/src/main/java/com/proyect/connection/ExtractionMetaDataDB.java
@@ -7,6 +7,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 
 import com.proyect.structure.Column;
+import com.proyect.structure.ForeignKey;
 import com.proyect.structure.Index;
 import com.proyect.structure.Table;
 
@@ -31,20 +32,34 @@ public class ExtractionMetaDataDB {
 	 */
 	
 	public ArrayList<Table> getTables(String schemaName) throws SQLException {
+		
 		String[] typeTable = {"TABLE"};		
 		ResultSet rsTables = metaData.getTables(null, schemaName, null, typeTable);
 		ArrayList<Table> tableList = new ArrayList<Table>();
+		String tableName = null;
+		ArrayList<Column> columns = null;
+		Table t;
+		
 		while(rsTables.next()) {
-			String tableName = rsTables.getString("TABLE_NAME");
-			ArrayList<Column> columns = getColumns(schemaName , tableName);
+			tableName = rsTables.getString("TABLE_NAME");
+			columns = getColumns(schemaName , tableName);
 			Index pk = getPrimaryKey(schemaName, tableName, columns);
-			Table t = new Table(tableName);
+			
+			t = new Table(tableName);
 			t.setColumns(columns);
 			
 			t.setPrimaryKey(pk);
 			tableList.add(t);
 			
 		}
+		//Set all foreign key for each table
+		for(int i = 0; i < tableList.size(); i++ ) {
+			
+			ArrayList<ForeignKey> fkList = getForeignsKeys(schemaName,tableList.get(i).getName(),tableList);
+			tableList.get(i).setForeignKeys(fkList);
+			
+		}
+		
 		
 		
 		return tableList;
@@ -52,12 +67,58 @@ public class ExtractionMetaDataDB {
 	 }
 	
 	
+	private ArrayList<ForeignKey> getForeignsKeys(String schemaName, String tableName, ArrayList<Table> tableList) throws SQLException {
+		ResultSet rsFKs = metaData.getImportedKeys(null , schemaName , tableName);
+		if(!rsFKs.next()) {
+			return null;
+		}
+		rsFKs.previous(); 	//back pointer to the begin.
+		
+		ArrayList<ForeignKey> fks = new ArrayList<>();
+		String oldFKname = "";
+		String  currentFKname= "";
+		Table t = getTable(tableName,tableList); 
+		Table refTable = null;
+		ForeignKey currentFK = null;
+		String fkColumnName = null;
+		String refColumnName = null;
+		String refTableName = null;
+		Column c = null;
+		Column refC = null;
+		while(rsFKs.next()) {
+			currentFKname = rsFKs.getString("FK_NAME");
+			refTableName = rsFKs.getString("PKTABLE_NAME");
+			fkColumnName = rsFKs.getString("FKCOLUMN_NAME");
+			refColumnName = rsFKs.getString("PKCOLUMN_NAME");
+			if(!currentFKname.equals(oldFKname)) {
+				oldFKname = currentFKname;
+				currentFK = new ForeignKey(currentFKname);
+				refTable = getTable(refTableName,tableList);
+				currentFK.setReference(refTable);
+				fks.add(currentFK);
+				
+			}
+			c = getColumn(fkColumnName,t.getColumns());
+			currentFK.getColumns().add(c);
+			refC = getColumn(refColumnName,refTable.getColumns());
+			currentFK.getReferencedColumns().add(refC);
+		}	
+		
+		return fks;
+	}
+
+
+
+
 	public ArrayList<Column> getColumns(String schemaName, String tableName) throws SQLException {	
+		
 		ResultSet rsColumns = metaData.getColumns(null , schemaName , tableName , null);
 		ArrayList<Column> columnList = new ArrayList<Column>();
+		String nameC = null;
+		String typeC = null;
 		while(rsColumns.next()) {
-			String nameC = rsColumns.getString("COLUMN_NAME");
-			String typeC = rsColumns.getString("TYPE_NAME");
+			nameC = rsColumns.getString("COLUMN_NAME");
+			typeC = rsColumns.getString("TYPE_NAME");
 			columnList.add(new Column(nameC , typeC));
 		}
 		
@@ -69,25 +130,32 @@ public class ExtractionMetaDataDB {
 		ResultSet rsColumnsPK = metaData.getPrimaryKeys(null , schemaName , tableName);
 		ArrayList<Column> columnPKList = new ArrayList<Column>();
 		String namePK = null;
+		Column c;
+		String nameC;
+		if(!rsColumnsPK.next()) {
+			return null;
+		}
+		else {
+			rsColumnsPK.previous();
+		}
 		while(rsColumnsPK.next()) {
 			if(namePK == null)
 				namePK = rsColumnsPK.getString("PK_NAME");
 			
-			String nameC = rsColumnsPK.getString("COLUMN_NAME");
+			nameC = rsColumnsPK.getString("COLUMN_NAME");
 			
-			Column c = getColumn(nameC,columnsTable);
+			 c = getColumn(nameC,columnsTable);
 			if(c != null)
 				columnPKList.add(c);
 			
 		}
-		
 		return new Index(namePK,columnPKList);
 		
 	}
 	
 	
 	// get a Column of a list of Columns given a name
-	private static Column getColumn(String name, ArrayList<Column> listC ) {
+	private static Column getColumn(String name , ArrayList<Column> listC ) {
 		for(Column c : listC) {
 			if(c.getName().equals(name))
 				return c;
@@ -96,6 +164,15 @@ public class ExtractionMetaDataDB {
 		
 	}
 	
+	//get a table of a list of Tables given a name
+	private static Table getTable(String name , ArrayList<Table> listT) {
+		for(Table t : listT) {
+			if(t.getName().equals(name))
+				return t;
+
+		}
+		return null;
+	}
 
 
 }
