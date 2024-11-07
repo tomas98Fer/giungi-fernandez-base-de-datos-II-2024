e21888611b7e5a50a68b63a090d8832ba90a9f12
giung1 <agus_98_jov@hotmail.com.ar>
2024-11-07 14:15:48 -0300

    test for class structure.Table added

---
+++ giungi-fernandez-proyecto/comparatorDBApp/src/test/java/com/project/structure/TableTest.java
@@ -0,0 +1,32 @@
+package com.project.structure;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+import java.util.ArrayList;
+import com.project.structure.*;
+
+public class TableTest {
+    
+    @Test
+    public void containsColumNameTest(){
+        Table table = new Table("table1");
+        Column column = new Column("column1, int4");
+        ArrayList<Column> columns = new ArrayList<>();
+        columns.add(column);
+        table.setColumns(columns);
+        assertTrue(table.containsColumnName(column)>=0);
+    }
+
+    @Test
+    public void notContainsColumNameTest(){
+        Table table = new Table("table1");
+        Column column = new Column("column1", "int4");
+        ArrayList<Column> columns = new ArrayList<>();
+        columns.add(column);
+        table.setColumns(columns);
+        Column column2 = new Column("column2", "varchar");
+        assertTrue(table.containsColumnName(column2)==-1);
+    }
+
+}
